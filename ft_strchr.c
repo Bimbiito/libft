58119886e1a2c100cf27a45d06997af3b86333e2
Bimbiito <fgallard@c2r19s5.42madrid.com>
2024-12-26 18:54:03 +0100

    c

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,32 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: fgallard <fgallard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 22:34:35 by marvin            #+#    #+#             */
-/*   Updated: 2024/12/24 22:34:35 by marvin           ###   ########.fr       */
+/*   Updated: 2024/12/26 18:49:01 by fgallard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-    while (*s != '\0')
-    {
-        if (*s == (char)c)
-        {
-            return (char *)s;
-        }
-        s++;
-    }
-    return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return ((char *)s);
+	}
+	return (NULL);
 }
 
 /*int main()
 {
-    const char *s = "Feliz Navidad Perras";
-    int c = 'Y';
+	const char *s = "Feliz Navidad Perras";
+	int c = 'Y';
 
-    printf("Aqui esta tu perdida: %c\n", *ft_strchr(s, c));
+	printf("Aqui esta tu perdida: %c\n", *ft_strchr(s, c));
 }*/
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,38 +6,36 @@
 /*   By: fgallard <fgallard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 12:43:27 by fgallard          #+#    #+#             */
-/*   Updated: 2024/12/17 16:29:51 by fgallard         ###   ########.fr       */
+/*   Updated: 2024/12/26 18:53:38 by fgallard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t n)
+size_t	ft_strlcat(char *dest, const char *src, size_t n) //size es la cantidad de bytes total del resultado, no la cantidad de bytes a compiar, tienes que restarle la cantidad de bytes de dest a size.
 {
-	size_t	dest_len = 0;
-	size_t	src_len = 0;
-	size_t	i = 0;
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	i;
+	
+	i = 0;
+	src_len = 0;
+	dest_len = 0;
 
 	while (dest[dest_len] != '\0')
-    {
-        dest_len++;
-    }
+    	 dest_len++;
 	while (src[src_len] != '\0')
     {
         src_len++;
 	if (n <= dest_len)
-    {
 		return (n + src_len);
-    }
-    }
+	}
 	while (src[i] != '\0' && dest_len + i < n - 1)
 	{
 		dest[dest_len + i] = src[i];
 		i++;
 	}
-
 	dest[dest_len + i] = '\0';
-
 	return (dest_len + src_len);
 }
 
